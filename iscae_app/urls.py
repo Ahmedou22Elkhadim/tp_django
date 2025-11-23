93a91782ecc50a93aaf61dde126ba1593164a0f4
ghareeb <ahmedouelkhadim09@gmail.com>
2025-11-23 15:06:26 +0100

    First deployement

--- iscae_app/urls.py
+++ iscae_app/urls.py
@@ -6,7 +6,7 @@ from iscae_app.views import list_item, say_hello_json,html_page , edit_item , su
 urlpatterns = [
     path('say_hello/', say_hello_json),
     path('html_page/',html_page),
-    path('list_item/',list_item , name='list_item'),
+    path('',list_item , name='list_item'),
     path('edit_item/<int:id>/edit', edit_item , name='edit_item'),
     path('delete_item/<int:id>/delete', supprimer_item  , name='delete_item'),
     path('items/create/', create_item, name='create_item'),
